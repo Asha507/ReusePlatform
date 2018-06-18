ea932aede1c659ae7f8312c18841781d837a7d4c
unknown <asha.polamarasetty@gmail.com>
2018-06-18 22:40:41 +0530

    css

--- ReuseUI/src/app/navbar/navbar.component.ts
+++ ReuseUI/src/app/navbar/navbar.component.ts
@@ -12,11 +12,21 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
     $(document).ready(function(){
-      $("#menu-toggle").click(function (e) {
+    /*   $("#menu-toggle").click(function (e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
-      });
+      }); */
   });
+  }
+
+  test(event){
+    let wrapper = event.currentTarget.closest('#wrapper');
+    if(wrapper.classList.contains('toggled')){
+      wrapper.classList.remove('toggled');
+    }
+    else{
+      wrapper.classList.add('toggled');
+    }
   }
 
 }
